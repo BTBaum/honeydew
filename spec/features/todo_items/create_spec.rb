7ea366505adeaaf499e7601fb07d5155f69e0389
Brandon <btbaum11@gmail.com>
2015-07-03 10:26:43 -0400

    Add test for creating todo items and associated code

---
+++ spec/features/todo_items/create_spec.rb
@@ -0,0 +1,25 @@
+require 'spec-helper'
+
+# Test creation of todo list
+describe "Adding todo items" do
+  let!(:todo_list) {TodoList.create(title: "Grocery list", description: "Groceries")}
+  
+  def visit_todo_list(list)
+    visit "/todo_lists"
+    within "#todo_list#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful with valid content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: "Milk"
+    click_button "Save"
+    expect(page).to have_content("Added todo list item.")
+    within("ul.todo_items") do
+      expect(page).to have_content("Milk")
+    end
+  end
+
+end
